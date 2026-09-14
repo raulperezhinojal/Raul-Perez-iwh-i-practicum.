4048f02046d56d5ff1313ae8932de59e3e42ab4c
Raul <raul.perez@gracia.lasalle.cat>
2026-09-14 10:07:11 +0200

    Agregar rutas de objetos personalizados y limpieza
    Implementa GET / para obtener y mostrar registros de objetos personalizados y agrega GET/POST /update-cobj para mostrar un formulario y crear nuevos registros de objetos personalizados a través de la API de HubSpot. Elimina el marcador de posición PRIVATE_APP_ACCESS y los ejemplos de contacto/actualización de muestra, centraliza los encabezados para usar process.env.PRIVATE_APP_ACCESS_TOKEN, y corrige el inicio del servidor para usar la variable de entorno PORT. Limpieza pequeña: elimina código comentado de TODO/muestras y variables sin usar.

--- index.js
+++ index.js
@@ -13,43 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
 const HUBSPOT_HEADERS = {
     Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS_TOKEN}`,
     'Content-Type': 'application/json'
 };
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 const OBJECT_TYPE = process.env.CUSTOM_OBJECT_ID;
 
-// * Code for Route 1 goes here
 // Ruta 1: GET / -> Mostrar tabla en la página principal
 app.get('/', async (req, res) => {
     const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}?properties=name,species,bio`;
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
         const response = await axios.get(url, { headers: HUBSPOT_HEADERS });
         const data = response.data.results;
         res.render('homepage', {
@@ -62,9 +37,6 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
 // Ruta 2: GET /update-cobj -> Mostrar formulario
 app.get('/update-cobj', (req, res) => {
     res.render('updates', {
@@ -83,20 +55,8 @@ app.post('/update-cobj', async (req, res) => {
             species,
             bio
         }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
     try {
         await axios.post(url, newRecord, { headers: HUBSPOT_HEADERS });
         res.redirect('/');
@@ -106,7 +66,6 @@ app.post('/update-cobj', async (req, res) => {
     }
 });
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
